dde1d38756f2b56b5e6dcf0f0ed4f6044e8b859e
Isaac Onyango <nderiony95@gmail.com>
2017-04-26 10:28:31 +0300

    add initial html file

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+jquery("h1").click function() {
+  alert("This is a header");
+});
+
+jquery("p").click function() {
+  alert("This is a paragraph");
+});
+
+jquery("img").click function() {
+  alert("This is a image");
+});
